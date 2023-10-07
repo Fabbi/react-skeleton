a9f7b6fd8fea3fc31669dcd4d2f1770299532f19
Fabian Schweinfurth <github@derfabbi.de>
2023-10-07 13:15:13 +0200

    removes junk

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// eslint-disable-next-line react-refresh/only-export-components
+const Wrapper = location.search.toLowerCase().includes("nostrict")
+  ? React.Fragment
+  : React.StrictMode;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Wrapper>
     <App />
-  </React.StrictMode>,
+  </Wrapper>,
 )
